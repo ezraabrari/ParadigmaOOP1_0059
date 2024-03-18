dc156bef302f9b29ab8c3adc1fd0c1ee0ff8dee2
ezraabrari <ezraabrari13@gmail.com>
2024-03-18 14:28:30 +0700

    looping menggunakan perulangan

---
+++ Contoh_Pertama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int arr[5]
+
+    for(i = 0; i<5; i++){
+        cout << i << "." << "ezra" << endl;
+    }
+    for (i = 0; i < 5; i++){
+        cout << "masukkan nama = ";
+        cin >> aii[i];
+    }
+
+    for (i = 0; i<5;i++){
+        cout << "nama ke - " << i << " : " << arr[i] << endl;
+    }
+}
