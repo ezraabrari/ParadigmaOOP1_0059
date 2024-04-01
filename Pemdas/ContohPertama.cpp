e1a31e13d70afdfb4f562538ded6fc90fcc22468
ezraabrari <ezraabrari13@gmail.com>
2024-04-01 14:43:55 +0700

    Memasukkan cout data mahasiswa ke layar kembali

--- Pemdas/ContohPertama.cpp
+++ Pemdas/ContohPertama.cpp
@@ -21,4 +21,10 @@ int main() {
 
     cout << "umur Mahasiswa : ";
     cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur; 
 }
