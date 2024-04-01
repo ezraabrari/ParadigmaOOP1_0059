3863fd0322879a534d431dbfbc7caf521fd0443b
ezraabrari <ezraabrari13@gmail.com>
2024-04-01 15:20:29 +0700

    Membuat Include dan namespace std

--- Pemdas/ContohPertama.cpp
+++ Pemdas/ContohPertama.cpp
@@ -1,30 +1,3 @@
 #include <iostream>
 using namespace std;
 
-struct Mahasiswa {
-    string nim;
-    string nama;
-    string alamat;
-    string umur;
-};
-
-int main() {
-    Mahasiswa mhs;
-    cout << "Nomor Mahasiswa : ";
-    cin >> mhs.nim;
-
-    cout << "Nama Mahasiswa : ";
-    cin >> mhs.nama;
-
-    cout << "Alamat Mahasiswa : ";
-    cin >> mhs.alamat;
-
-    cout << "umur Mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout << "\n NIM : " << mhs.nim;
-    cout << "\n Nama : " << mhs.nama;
-    cout << "\n Alamat : " << mhs.alamat;
-    cout << "\n Umur : " << mhs.umur; 
-}
