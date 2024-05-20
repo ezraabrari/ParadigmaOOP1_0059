79264d89c1ee1b2775b5d3715d9a81843734b9ef
ezraabrari <ezraabrari13@gmail.com>
2024-05-20 14:36:53 +0700

    Membuat cout masing masing variable

--- Paradigma oop/MemberInit.cpp
+++ Paradigma oop/MemberInit.cpp
@@ -9,7 +9,12 @@ private:
 
 public:
     mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
-        
+
     }
 
+    ~mahasiswa() {
+        cout << "Id     = " << id << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << "Nilai  = " << nilai << endl;
+    }
 };
