26edf2c40557492a2ab4d8e5e67a9d35f5156933
ezraabrari <ezraabrari13@gmail.com>
2024-05-20 17:48:54 +0700

    membuat prosedur tambah dokter dan cetak dokter

--- Paradigma oop/asosiasi/Asosiasi.cpp
+++ Paradigma oop/asosiasi/Asosiasi.cpp
@@ -31,3 +31,15 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter (dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien\"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
