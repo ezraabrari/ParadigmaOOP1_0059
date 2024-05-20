379fac0fa296da9e4abdaa2e08904b23ebb88afe
ezraabrari <ezraabrari13@gmail.com>
2024-05-20 15:02:45 +0700

    membuat prosedur ibu tambah anak

---
+++ Paradigma oop/Agregasi/HeaderIbu.cpp
@@ -0,0 +1,22 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada \n";
+    }
+    ~ibu() {
+        cout << "ibu \"" << nama << "\" ada \n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+}
